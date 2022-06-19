a37a67ee4a5fcc61457db32741f5b05b8d58c160
Brent Son <brentson00@gmail.com>
2022-06-18 18:09:25 -0700

    added new textline

--- main.c
+++ main.c
@@ -2,12 +2,10 @@
 
 int main(void){
 
-<<<<<<< HEAD
     printf("This is for the first commit.\n");
     printf("This is for the second commit.\n");
 
     
-=======
     int num1, num2, sum;
 
     printf("Please enter two numbers:\n");
@@ -17,7 +15,7 @@ int main(void){
 
     printf("The sum of these two number is %d\n", sum);
     printf("If this works then I understand Git to a certain degree.\n");
->>>>>>> development
+    printf("This is a test to see if pushing to GitHub worked or not.\n");
 
     return 0;
 }
