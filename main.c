8cc3f39e12a1d987f848f531c94d54f7acaa0c29
Brent Son <brentson00@gmail.com>
2022-06-18 18:21:44 -0700

    new test

--- main.c
+++ main.c
@@ -8,13 +8,14 @@ int main(void){
     
     int num1, num2, sum;
 
-    printf("Please enter two numbers:\n");
-    scanf("%d %d\n", &num1, &num2);
+   // printf("Please enter two numbers:\n");
+    //scanf("%d %d\n", &num1, &num2);
 
     sum = num1 + num2;
 
-    printf("The sum of these two number is %d\n", sum);
+    //printf("The sum of these two number is %d\n", sum);
     printf("If this works then I understand Git to a certain degree.\n");
+    printf("This is a second test to see if i pushed to GitHub correctly.");
 
     return 0;
 }
