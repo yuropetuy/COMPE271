c61e6ebad4d3d9bdbfee13d7836522b4ff2072f2
Brent Son <brentson00@gmail.com>
2022-06-24 22:17:58 -0700

    added 2D for field of play with temp sprite for car

--- main.c
+++ main.c
@@ -1,12 +1,37 @@
 #include <stdio.h>
+#include <windows.h>
+#include <stdlib.h>
+#include <conio.h>
+#include <time.h>
 
-#define N 40
-#define M 90
+#define N 10
+#define M 40
 
 int i,j;
+int x,y;
+int car;
+int field[N][M];
 
+void generateField(){
+
+    //empty field
+    for(i = 0; i < N; i++){
+        for(j = 0; j < M; j++){
+            field[i][j] = 0;
+        }
+    }
+
+    //where the car is
+    x = N/2;
+    y = M/2;
+    car = 1;
+
+    field[x][y] = car;
+}
 
 void print(){
+
+    //top row
     for(i = 0; i < M+1; i++){
         if (i==0)
             printf("%c", 201);
@@ -15,9 +40,35 @@ void print(){
         else
             printf("%c", 205);
     }
+
+    printf("\n");
+    
+    //middle rows
+    for(i = 0; i < N; i ++){
+        printf("%c", 186);
+        for(j = 0; j < M-1; j++){
+            if(field[i][j] == 0)
+                printf("~");
+            else if(field[i][j] == car)
+                printf("0");
+        }
+        printf("%c\n", 186);
+    }
+
+    //bottom row
+    for(i = 0; i < M+1; i ++){
+        if (i ==0)
+            printf("%c", 200);
+        else if (i==M)
+            printf("%c", 188);
+        else
+            printf("%c", 205);
+    }
 }
 
 int main(){
+
+    generateField();
 
     print();
 
